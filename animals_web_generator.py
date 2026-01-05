e6356e969ac05203c6922489e64b6f369e248f0f
ibibibi83 <sefaibrahim83@hotmail.de>
2026-01-05 11:15:10 +0100

    added functionality for html creation

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,8 +1,29 @@
 import data_fetcher
 
-animal_name = input("Please enter an animal: ")
-data = data_fetcher.fetch_data(animal_name)
+def load_data(file_path):
+  """ Loads a HTML file """
+  with open(file_path, "r") as handle:
+    return handle.read()
+
+def build_html_data(animals_data):
+    output = ""
+    for animal in animals_data:
+      output += f"name:{animal["name"]}\n"
+      output +=f"diet:{animal["characteristics"]["diet"]}\n"
+      output +=f"location:{animal["locations"][0]}\n"
+      if animal["characteristics"].get("type"):
+        output += f"type:{animal["characteristics"].get("type")}\n"
+      output += "\n"
+
+    return output
 
 
-for name in data_fetcher.names:
-    print(name)
+if __name__ == '__main__':
+    template = load_data("animals_template.html")
+    animal_name = input("Please enter an animal: ")
+    data = data_fetcher.fetch_data(animal_name)
+    html_animal = build_html_data(data)
+    final_data_animal = template.replace("__REPLACE_ANIMALS_INFO__", html_animal)
+
+    with open("index.html", "w") as handle:
+        handle.write(final_data_animal)
